490286ba842c656980d8f20003d7469a56f21d11
roylu <roylu@uw.edu>
2020-02-27 14:19:33 -0800

    update

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import re
 import http.client
 #import config
 import json
@@ -22,12 +23,38 @@ def thres_red(text, val, thres=1):
     else:
         return colored(text, 'white')
 
-def run_cmd(args):
-    if isinstance(args, str):
-        args = args.split()
-    p = subprocess.run(args, stdout=subprocess.PIPE, check=True)
-    return p.stdout.decode().strip()
 
 def uniq(arr):
     # ref: https://mail.python.org/pipermail/python-dev/2017-December/151283.html
     return list(dict.fromkeys(arr))
+
+
+def run_cmd(args):
+    try:
+        out = subprocess.Popen(args,
+                               stdout=subprocess.PIPE, 
+                               stderr=subprocess.PIPE)
+    except Exception as e:
+        print('[Error]', e)
+        exit(1)
+    stdout, stderr = out.communicate()
+    return stdout.decode("utf-8")
+
+
+def clean_duplicate_nodes(nodes):
+    new_nodes = []
+    existing_node = set()
+    for node in nodes:
+        name = node['Machine']
+        if name not in existing_node: 
+            existing_node.add(name)
+            new_nodes.append(node)
+    return new_nodes
+
+
+def get_node_names():
+    stdout = run_cmd(['condor_status', '-long', '-json'])
+    nodes = json.loads(stdout)
+    nodes = clean_duplicate_nodes(nodes)
+    nodes = [node['Machine'].split('.')[0] for node in nodes]
+    return nodes
