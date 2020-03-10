dfaf45f0433eba5c404ea5bf16525b52bdbf715a
roylu <roylu@uw.edu>
2020-03-10 12:20:45 -0700

    update

--- gpuinfo.sh
+++ gpuinfo.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/bash
 
-python=/g/ssli/roylu/bin/python3 
-gpu_info_py=/g/ssli/roylu/roy_condor_wrapper/gpuinfo.py
+gpu_info_py=/g/ssli/sw/roylu/roy_condor_wrapper/gpuinfo.py
 
 while true
 do
-    $python $gpu_info_py
+    /g/ssli/sw/roylu/bin/python3 $gpu_info_py
     sleep 1
 done
