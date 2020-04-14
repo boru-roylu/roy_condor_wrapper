59fd83afbb6a5bd852c4849c2120a4bbc5a08604
roylu <roylu@uw.edu>
2020-04-14 16:24:27 -0700

    python binding and remove redirectory

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 cpu_min = 1
 cpu_max = 40
-mem_min = 0.1
+mem_min = 1
 mem_max = 376
 gpu_max = 8
 gpu_models = ['TITAN X (Pascal)', 'TAITAN Xp', '2080Ti']
--- utils.py
+++ utils.py
@@ -3,8 +3,7 @@
 import os
 import re
 import csv
-import http.client
-from io import StringIO
+import glob
 import json
 import subprocess
 from termcolor import colored
@@ -12,6 +11,7 @@ from pathlib import Path
 import portalocker
 
 from config import *
+from normalize_tial_path import normalize_tial_path
 
 def colored_by_ratio(text, ratio, thres=[0.5, 0.999999]):
     if ratio < thres[0]:
@@ -75,3 +75,38 @@ def read_gpu_info(hostname):
     with portalocker.Lock(gpu_info_path, 'r', timeout=5) as f:
         gpus = json.load(f)
     return gpus['gpus']
+
+
+def create_run_script(tmpdir, cmd):
+    script_path = os.path.join(tmpdir, 'run.sh')  
+    with open(script_path, 'w') as f:
+        print('#! /bin/sh', file=f)
+        print(' '.join(real_cmd), file=f)
+    return script_path
+
+
+def create_submit_file(condor_params, tmpdir):
+    submit_file_path = os.path.join(tmpdir, 'submit_file')
+    with open(submit_file_path, 'w') as f:
+        for k, v in condor_params.items():
+            print(f'{k:20}= {v}', file=f)
+        print('Queue', file=f)
+    return submit_file_path
+
+
+def get_executable_cmd(cmd):
+    proc = subprocess.Popen(['which', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if stdout:
+        return stdout.decode("utf-8").strip(), True
+    else:
+        cmd = normalize_tial_path(os.path.realpath(cmd))
+        if os.path.exists(cmd):
+            return cmd, True
+        else:
+            return cmd, False
+
+
+def remove_files_in_dir(dir_path):
+    for path in glob.glob(os.path.join(dir_path, '*')):
+        os.remove(path)
